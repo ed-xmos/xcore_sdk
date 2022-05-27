c3ecc753b74eb25601edffe205ce91a88c1138cf
Ed <ed@xmos.com>
2022-05-27 14:10:56 +0100

    Working 1Mbps UART loopback bare-metal demo

--- examples/bare-metal/explorer_board/src/demos/uart_demo.c
+++ examples/bare-metal/explorer_board/src/demos/uart_demo.c
@@ -16,24 +16,30 @@
 
 void uart_rx_demo(uart_rx_t* uart_rx_ctx)
 {
-    return;
     uint8_t expected = 0;
+    hwtimer_t tmr = hwtimer_alloc();
+
     while(1) {
         uint8_t rx = uart_rx(uart_rx_ctx);
         if(rx != expected){
-            // debug_printf("UART data error, expected %d got %d\n", expected, rx);
+            debug_printf("UART data error, expected: %d got: %d\n", expected, rx);
         }
         expected++;
+        hwtimer_delay(tmr, 20); //Max slippage delay 200ns
     }
 }
+
 
 void uart_tx_demo(uart_tx_t* uart_tx_ctx)
 {
     uint8_t tx_data = 0;
+    debug_printf("Starting tx ramp test @ %ubaud..\n", XS1_TIMER_HZ / uart_tx_ctx->bit_time_ticks);
+
+    hwtimer_t tmr = hwtimer_alloc();
+    hwtimer_delay(tmr, XS1_TIMER_KHZ);
+
     while(1) {
         uart_tx(uart_tx_ctx, tx_data);
-        // debug_printf("%d\n", tx_data);
         tx_data+=1;
-        // delay_milliseconds(1);
     }   
 }
